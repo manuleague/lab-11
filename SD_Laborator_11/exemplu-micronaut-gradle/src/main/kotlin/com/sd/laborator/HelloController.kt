6a08923c2e2a7de45eb0ddd0de1d8098e6cb1590
manuleague <danalacheemanuel@gmail.com>
2025-06-20 14:50:19 +0300

    11

---
+++ SD_Laborator_11/exemplu-micronaut-gradle/src/main/kotlin/com/sd/laborator/HelloController.kt
@@ -0,0 +1,13 @@
+package com.sd.laborator
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+
+@Controller("/hello")
+class HelloController {
+    @Get(uri="/", produces=["text/plain"])
+    fun index(): String {
+        return "Hello from Micronaut!"
+    }
+}
